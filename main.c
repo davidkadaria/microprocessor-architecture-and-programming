0d04df88b5dbe42a7ca25c14104ffbb03661e70e
David Kadaria <94402615+davidkadaria@users.noreply.github.com>
2026-04-16 15:56:25 +0400

    Implement ALU functionality and control logic
    Added ALUControl variable and implemented ALU and ALU_Decoder functions for arithmetic operations.

--- main.c
+++ main.c
@@ -8,14 +8,14 @@ int IS_EXIT = 0;
 int AAA = 0;
 
 // Global state variables
-uint32_t RegWrite  = 0;
-uint32_t ImmSrc    = 0;
-uint32_t ALUSrc    = 0;
-uint32_t MemWrite  = 0;
-uint32_t ResultSrc = 0;
-uint32_t Branch    = 0;
-uint32_t ALUOp     = 0;
-
+uint32_t RegWrite   = 0;
+uint32_t ImmSrc     = 0;
+uint32_t ALUSrc     = 0;
+uint32_t MemWrite   = 0;
+uint32_t ResultSrc  = 0;
+uint32_t Branch     = 0;
+uint32_t ALUOp      = 0;
+uint32_t ALUControl = 0;
 
 
 uint32_t Extract_Bits(uint32_t input, uint32_t from, uint32_t to)
@@ -37,6 +37,39 @@ int32_t Extend_Imm(uint32_t instruction, uint8_t immSrc)
 
         default:
             return 0;
+    }
+}
+
+int32_t ALU(int32_t A, int32_t B, uint32_t ALUControl)
+{
+    switch (ALUControl)
+    {
+        case 0: // add
+            return A + B;
+
+        case 1: // sub
+            return A - B;
+
+        default:
+            return 0;
+    }
+}
+
+void ALU_Decoder(uint32_t ALUOp)
+{
+    switch (ALUOp)
+    {
+        case 0: // 00 : add
+            ALUControl = 0; // 000
+            break;
+
+        case 1: // 01 : subtract
+            ALUControl = 1; // 001
+            break;
+
+        default:
+            ALUControl = 0;
+            break;
     }
 }
 
@@ -114,15 +147,31 @@ void* SOFT_CPU_THREAD()
     // prerequisites stop
     
     uint32_t instruction = Instruction_Memory[PC];
+    
+    uint32_t rs1 = Extract_Bits(instruction, 15, 19);
+    uint32_t rs2 = Extract_Bits(instruction, 20, 24);
+    uint32_t rd  = Extract_Bits(instruction, 7, 11);
 
     Main_Decoder(instruction);
     
     int32_t imm = Extend_Imm(instruction, ImmSrc);
+    
+    ALU_Decoder(ALUOp);
+    
+    int32_t SrcA = Register_File[rs1];
+    int32_t SrcB = (ALUSrc) ? imm : Register_File[rs2];
+    
+    int32_t alu_result = ALU(SrcA, SrcB, ALUControl);
 
     printf("Instruction: 0x%08X\n", instruction);
     printf("RegWrite=%u | ImmSrc=%u | ALUSrc=%u | MemWrite=%u | ResultSrc=%u | Branch=%u | ALUOp=%u\n",
        RegWrite, ImmSrc, ALUSrc, MemWrite, ResultSrc, Branch, ALUOp);
     printf("Immediate = %d (0x%X)\n", imm, imm);
+    printf("rs1 = x%u (%d)\n", rs1, Register_File[rs1]);
+    printf("rs2 = x%u (%d)\n", rs2, Register_File[rs2]);
+    printf("rd  = x%u\n", rd);
+    printf("ALUControl = %u\n", ALUControl);
+    printf("ALU Result = %d (0x%X)\n", alu_result, alu_result);
 
     // for (int i = 0; i < 2; i++)
     // {
@@ -156,7 +205,7 @@ void* TASK_keyboard()
 
 int main()
 {
-    const int NUM_THREADS = 3;
+    const int NUM_THREADS = 1;
     pthread_t threads[NUM_THREADS];
     //---------------------------------------------
     if (pthread_create(&threads[0], NULL, SOFT_CPU_THREAD, NULL) != 0)
